eb19095473217ce80a55c87a846765061632e6e1
Youssef Hamed <yousiefhamed1@gmail.com>
2024-07-06 00:01:30 +0300

    deployment on railway

--- server.js
+++ server.js
@@ -11,9 +11,11 @@ app.use(cors());
 app.use("/api/products", productsRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(
+    "mongodb+srv://yousiefhamed1:YC0BSHSEV2AJGpfQ@grandpa-ali-cluster.uam6mme.mongodb.net/?retryWrites=true&w=majority&appName=grandpa-ali-cluster"
+  )
   .then(() => {
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT || 5000, () => {
       console.log("connected to db & listening on port ", process.env.PORT);
     });
   })
